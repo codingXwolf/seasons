f4a941c7d248f3bf0dcbd3f91cceb750db269b6b
Jaime Ortiz <jaimeortiz@Jaimes-MacBook-Pro.local>
2018-12-12 23:32:02 -0800

    changed to a class component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition( //get current users location. First argument is a callback function with location, second is callback function for error.
-        (position) => console.log(position),
-        (err) => console.log(err)
-    )
-    return <div>Orale Ese!</div>
-};
 
-ReactDOM.render(
-    <App/>,
-    document.querySelector('#root')
-);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lat: null //so a value of null means essentially we don't know what the latitude or number is it yet we will know eventually.
+        };
+
+        window.navigator.geolocation.getCurrentPosition( //get current users location. First argument is a callback function with location, second is callback function for error.
+            (position) => {
+                this.setState({
+                    lat:position.coords.latitude
+                })
+            },
+            (err) => console.log(err)
+        )
+    }
+
+    render() {
+        return <div>Latitude: {this.state.lat}</div>;
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
