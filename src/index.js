8bbe930c0dfd59bf86c94b32cfe198b622eb8815
Jaime Ortiz <jaimeortiz@Jaimes-MacBook-Pro.local>
2018-12-12 22:27:10 -0800

    added user current location

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    window.navigator.geolocation.getCurrentPosition( //get current users location. First argument is a callback function with location, second is callback function for error.
+        (position) => console.log(position),
+        (err) => console.log(err)
+    )
+    return <div>Orale Ese!</div>
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);
