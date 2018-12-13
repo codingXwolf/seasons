c09f7c54a49608fd1cd11360480bd8c5d8abffd9
Jaime Ortiz <jaimeortiz@Jaimes-MacBook-Pro.local>
2018-12-13 15:47:25 -0800

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -8,20 +8,29 @@ class App extends React.Component {
 
         this.state = {
             lat: null //so a value of null means essentially we don't know what the latitude or number is it yet we will know eventually.
+            ,errorMessage: ''
         };
 
         window.navigator.geolocation.getCurrentPosition( //get current users location. First argument is a callback function with location, second is callback function for error.
-            (position) => {
-                this.setState({
-                    lat:position.coords.latitude
-                })
+            position => {
+                this.setState({lat:position.coords.latitude})
             },
-            (err) => console.log(err)
+            err => {
+                this.setState({errorMessage: err.message})
+            }
         )
     }
 
     render() {
-        return <div>Latitude: {this.state.lat}</div>;
+        if(this.state.errorMessage && !this.state.lat){
+            return <div> Error: {this.state.errorMessage}</div>
+        } 
+
+        if(!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading!</div>;
     }
 }
 
