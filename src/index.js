dc472a260795b44299299e94e81afab294c339ac
Jaime Ortiz <jaimeortiz@Jaimes-MacBook-Pro.local>
2018-12-13 17:39:36 -0800

    added logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component {
@@ -10,24 +11,22 @@ class App extends React.Component {
             lat: null //so a value of null means essentially we don't know what the latitude or number is it yet we will know eventually.
             ,errorMessage: ''
         };
+    }
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition( //get current users location. First argument is a callback function with location, second is callback function for error.
-            position => {
-                this.setState({lat:position.coords.latitude})
-            },
-            err => {
-                this.setState({errorMessage: err.message})
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         )
     }
 
     render() {
         if(this.state.errorMessage && !this.state.lat){
-            return <div> Error: {this.state.errorMessage}</div>
+            return <div> Error: {this.state.errorMessage}</div>;
         } 
 
         if(!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
 
         return <div>Loading!</div>;
